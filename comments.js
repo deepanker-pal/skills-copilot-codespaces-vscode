00c68406f542b157c9ad8e9b6bee047e87f6948b
deepanker-pal <deepanker.pal@fundserv.com>
2024-12-31 16:14:08 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,34 @@
+// create web server
+// respond to request
+// read data from file and send it back to the client
+// 1. load http module
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+// 2. create server
+http.createServer(function (req, res) {
+    // parse the request containing file name
+    var pathname = url.parse(req.url).pathname;
+    // print the name of the file for which request is made
+    console.log("Request for " + pathname + " received.");
+    // read the requested file content from file system
+    fs.readFile(pathname.substr(1), function (err, data) {
+        if (err) {
+            console.log(err);
+            // HTTP Status: 404 : NOT FOUND
+            // Content Type: text/plain
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+        } else {
+            // Page found
+            // HTTP Status: 200 : OK
+            // Content Type: text/plain
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            // write the content of the file to response body
+            res.write(data.toString());
+        }
+        // send the response body
+        res.end();
+    });
+}).listen(8081);
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
